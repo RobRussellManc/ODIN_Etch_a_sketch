d5252b5ba1f11cbad8c306b7aa697a3a9fa5b3f3
Robert <94055223+RobRussellManc@users.noreply.github.com>
2024-09-01 11:37:24 +0100

    Added random colors

--- javascript.js
+++ javascript.js
@@ -1,6 +1,25 @@
 
 let main_box = document.querySelector("#main_container");
 
+const brightColors = [
+    "#FF5733", // Bright Orange-Red
+    "#33FF57", // Bright Lime Green
+    "#3357FF", // Bright Blue
+    "#FF33A1", // Bright Pink
+    "#FFFF33", // Bright Yellow
+    "#FF33FF", // Bright Magenta
+    "#33FFF3", // Bright Cyan
+    "#FF8C33", // Bright Orange
+    "#33FF8C", // Bright Mint Green
+    "#8C33FF"  // Bright Purple
+];
+
+function getRandomColor() {
+    // Get a random index from the brightColors array
+    const randomIndex = Math.floor(Math.random() * brightColors.length);
+    // Return the color at that index
+    return brightColors[randomIndex];
+}
 
 function create_grid(size) {
     total_squares = size * size
@@ -14,10 +33,13 @@ function create_grid(size) {
         
         div.style.height = (100 / size) + '%';
         div.style.width = (100 / size) + '%';
+
+        
         
         // add mouse over event
         div.onmouseover = function() {
             div.classList.add('divbox_over');
+            div.style.backgroundColor = getRandomColor();
         }
         // append to the main container 
         main_box.appendChild(div)
@@ -43,7 +65,6 @@ button_input.addEventListener("click", () => {
         }
         // Create new grid
         create_grid(userInput)
-    
     }
 })
 
